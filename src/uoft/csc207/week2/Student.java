28d8b167a018d0fafb7c16cfe27fc373a16b2cbe
lauraz25 <laurazhan21@gmail.com>
2023-09-12 14:59:29 -0400

    added studentid attribute in Student class

--- src/uoft/csc207/week2/Student.java
+++ src/uoft/csc207/week2/Student.java
@@ -1,7 +1,14 @@
 package uoft.csc207.week2;
 
 public class Student extends Person {
+    private final String studentid;
+    /*
+    final means you can't reassign it. When Java autocreated this, it made it final because
+    there's no method to reassign it.
+    */
+
     public Student(String[] name, String utorid, String studentid) {
         super(name, utorid);
+        this.studentid = studentid;
     }
 }
